ba0b4dd738681c50cf882c1cd1fb084d6f2c39de
victor.guidi <vsantosg4072@gmail.com>
2022-08-07 19:13:12 -0300

    Fixed Nginx Proxy Manager image

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
-export class AppController { }
+export class AppController {
+  @Get()
+  getHello(): string {
+    return "hello world"
+  }
+}
